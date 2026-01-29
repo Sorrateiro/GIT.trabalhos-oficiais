bac857e5a37a84499c1fcf7501f11be687cdf224
Henrique <151760683+Sorrateiro@users.noreply.github.com>
2026-01-29 18:20:07 -0300

    l

--- php/index.php
+++ php/index.php
@@ -1,4 +1,12 @@
-<?php
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contato</title>
+</head>
+<body>
+    <?php
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     $nome = $_POST['nome'];
     $email = $_POST['email'];
@@ -17,3 +25,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     }
 }
 ?>
+    
+</body>
+</html>
