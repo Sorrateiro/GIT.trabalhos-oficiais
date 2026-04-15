0fb9ce8dd159ac8fccc3fe4d76faeb86f9603548
Henrique <151760683+Sorrateiro@users.noreply.github.com>
2026-04-14 22:10:54 -0300

    rinnovare menu atualizado

--- script/script2.js
+++ script/script2.js
@@ -2,3 +2,26 @@
         const lista = document.getElementById('lista' + index);
         lista.style.display = (lista.style.display === 'none' || lista.style.display === '') ? 'block' : 'none';
     }
+
+const Hamburguer = document.getElementById('Hamburguer');
+const menu = document.getElementById('menu');
+
+function Menuclick() {
+    if (menu.style.display === 'none' || menu.style.display === '') {
+        menu.style.display = 'block';
+    } else {
+        menu.style.display = 'none';
+    } 
+}
+function MudeDisplay() {
+    if (window.innerWidth > 768) {
+        menu.style.display = 'block';
+    } else {
+        menu.style.display = 'none';
+    }
+}
+function oculto() {
+    if (window.innerWidth <= 768) {
+        menu.style.display = 'none';
+    }
+}
